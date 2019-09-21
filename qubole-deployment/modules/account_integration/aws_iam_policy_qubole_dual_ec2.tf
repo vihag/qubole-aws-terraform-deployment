5ea0f3bb11531e8efa5d09f20128955bb8491502
vihag <vihag.gupta@gmail.com>
2019-09-21 13:14:49 +0800

    documentation on account integration module tf files complete

--- qubole-deployment/modules/account_integration/aws_iam_policy_qubole_dual_ec2.tf
+++ qubole-deployment/modules/account_integration/aws_iam_policy_qubole_dual_ec2.tf
@@ -5,6 +5,7 @@ Creates a Custom IAM Policy to provide permissions for the EC2 Service to
 
  This is for the following reason:
  1. Qubole uses the a Cross Account Role with the IAM policy to spin up clusters using EC2 services
+ 2. This policy can be edited to suit the Compute needs for the Dual IAM role which will applied to the clusters as INSTANCE PROFILE
 
  Caveats:
  1. The customer should ensure that the listed permissions are not taken away as it might result in loss of functionality
--- qubole-deployment/modules/account_integration/aws_iam_role_qubole_cross_account.tf
+++ qubole-deployment/modules/account_integration/aws_iam_role_qubole_cross_account.tf
@@ -1,3 +1,14 @@
+/*
+Creates an IAM role, called Cross Account Role
+1. Create a cross account accessible role
+2. Cross account accessibility is provided using the trust policy in which the Qubole Principal AWS Account ID + Customer specific-generated
+   EXTERNAL ID is used to authorize use with confidence
+
+ This is for the following reason:
+ 1. We need to authorize Qubole to be able to use this Role
+ 2. This role, with its trust policy allows the Qubole Principal AWS Account + EXTERNAL ID to access this with confidence
+*/
+
 data "template_file" "qubole_cross_account_role_trust_template" {
   template = file("${path.module}/policies/qubole_ca_role_trust_doc.json")
   vars = {
